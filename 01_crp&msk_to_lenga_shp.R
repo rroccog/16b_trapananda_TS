01d6e5ec8ecf0f8d8f2a0bb2db738a0afb170f1c
rroccog <r.roccogatica@gmail.com>
2018-01-22 11:57:04 -0300

    crop & mask MODIS -> SubCuenca Mañihuales

--- 01_crp&msk_to_lenga_shp.R
+++ 01_crp&msk_to_lenga_shp.R
@@ -1,11 +1,11 @@
 library(raster)
 library(rgdal)
 
-files <- list.files("~/ROCCO/PROJECTS/16b_trapananda_TS/00_MOD13_QA_testt/00_rawdata", pattern = glob2rx("*.tif"), full.names =T) 
+files <- list.files("~/PROJECTS/16b_trapananda_TS/00_MOD13_QA_testt/00_rawdata", pattern = glob2rx("*.tif"), full.names =T) 
 r1 <- raster(files[1])
 
-lenga <- readOGR(dsn = path.expand("~/ROCCO/PROJECTS/16b_trapananda_TS/shp"), layer = "Lenga_distribution_UTMWGS8419S")
-cuenca <- readOGR(dsn = path.expand("~/ROCCO/PROJECTS/16b_trapananda_TS/shp"), layer = "subcuenca_manihuales")
+lenga <- readOGR(dsn = path.expand("~/PROJECTS/16b_trapananda_TS/shp"), layer = "Lenga_distribution_UTMWGS8419S")
+cuenca <- readOGR(dsn = path.expand("~/PROJECTS/16b_trapananda_TS/shp"), layer = "subcuenca_manihuales")
 
 
 prj <- projection(r1)
@@ -14,7 +14,7 @@ cuencaprj <- spTransform(cuenca, "+proj=utm +zone=19 +south +ellps=WGS84 +units=
 projection(r1)
 
 names <- substr(files, 79, 94)
-dir <- "~/ROCCO/PROJECTS/16b_trapananda_TS/00_MOD13_QA_testt/02_msk_lenga"
+dir <- "~/PROJECTS/16b_trapananda_TS/00_MOD13_QA_testt/02_msk_lenga"
 
 outnames <- paste(dir, "/", "SC_manihuales_", names, "_lenga.tif", sep = "")
 
